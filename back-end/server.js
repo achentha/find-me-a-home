8e870dda3d8ea29292a541a63050e17c2c23e8f3
Apichai Chenthanakij <badchen@yahoo.com>
2017-11-01 12:44:52 -0700

    more fixed

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,22 @@
+require('dotenv').config();
+
+
 var express = require('express');
 var app = express();
+const path = require('path');
+
 var routes = require('./config/routes.js');
 
 //CORS setup to allow other ports from this host
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-  next();
-});
-
+//Only needed if not on Heroku/prod
+ if(!process.env.DYNO) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+  });
+}
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
@@ -36,6 +43,12 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./config/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.use(function(req, res, next) {
   console.log("got a request", req.method, req.url);
